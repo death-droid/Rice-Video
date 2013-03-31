a24e21637c984b78ac42c88f1a1778251a8c463c
Emmet Young <death-droid@hotmail.com>
2013-04-01 01:16:28 +1100

    Remove an identical condition in the same if statement - Lioncash

--- Texture/TextureManager.h
+++ Texture/TextureManager.h
@@ -101,7 +101,6 @@ public:
 			Size == sec.Size &&
 			Pitch == sec.Pitch &&
 			bSwapped == sec.bSwapped &&
-			TLutFmt == sec.TLutFmt &&
 			mirrorS == sec.mirrorS &&
 			mirrorT == sec.mirrorT &&
 			clampS == sec.clampS &&
