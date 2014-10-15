75400ccc2cdfe71c6c3963fcdb14d2f3e5793533
Simon <homer12345@gmail.com>
2014-10-14 18:55:36 -0700

    Add x64 to project files
    Also push WINVER to windows 7 for correct defines and lib imports

--- stdafx.h
+++ stdafx.h
@@ -14,9 +14,9 @@
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0601
 #include <stdio.h>
-#define WINVER 0x0400
+#define WINVER 0x0601
 #include <windows.h>
 #include <windowsx.h>			// Button_* etc
 #include <shlwapi.h>
